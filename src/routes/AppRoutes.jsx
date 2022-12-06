06dfb4daaa930586ddd8504e32ac3cb915415652
victor moreno <oliverio89@gmail.com>
2022-12-06 19:23:34 +0100

    last merge day2

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,9 @@
 import { Routes, Route } from "react-router-dom"
 import LoginPage from "../pages/LoginPage/LoginPage"
 import SignupPage from './../pages/SignupPage/SignupPage'
-<<<<<<< HEAD
-import PostListPage from '../pages/PostPage/PostPage'
-=======
 import PostListPage from './../pages/post/postPage'
 import PrivateRoute from "./PrivateRoute"
 
->>>>>>> 335e0d07a72487dff01e5fb0e309a44eb3dbd75a
 
 
 
@@ -18,14 +14,11 @@ const AppRoutes = () => {
         <Routes>
             <Route path="/" element={<p>Circle</p>} />
             <Route path="/post" element={<PostListPage />} />
-            <Route path="/profile" element={<PrivateRoute />} />
             <Route path="/aboutus" element={<p>Contacta con Nosotros</p>} />
             <Route path="/acceder" element={<LoginPage />} />
-            <Route path="/*" element={<h1>404</h1>} />
+            <Route path="/" element={<h1>404</h1>} />
             <Route path="/registro" element={<SignupPage />} />
-<<<<<<< HEAD
-=======
-            {/* <Route path="/registro" element={<p>HIIIII</p>} /> */}
+
 
             <Route element={<PrivateRoute />}>
                 <Route path="/profile" element={<p>Perfil</p>} />
@@ -33,7 +26,6 @@ const AppRoutes = () => {
             </Route>
 
 
->>>>>>> 335e0d07a72487dff01e5fb0e309a44eb3dbd75a
         </Routes>
     )
 }
