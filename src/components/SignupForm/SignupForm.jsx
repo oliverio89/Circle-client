ce3b01e9f7a1632d6e2901e4c5bec40dd92ef51b
YanxiaWu <yanxiawusilva@gmail.com>
2022-12-06 11:52:01 +0100

    SigunpForm

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -12,7 +12,10 @@ const SignupForm = () => {
     const [signupData, setSignupData] = useState({
         username: '',
         email: '',
-        password: ''
+        password: '',
+        name: '',
+        imageUrl: '',
+        bio: ''
     })
 
     const handleInputChange = e => {
@@ -40,14 +43,14 @@ const SignupForm = () => {
 
 
 
-    const { username, password, email } = signupData
+    const { username, password, email, name, imageUrl, bio } = signupData
 
     return (
 
         <Form onSubmit={handleSubmit}>
 
             <Form.Group className="mb-3" controlId="username">
-                <Form.Label>Nombre de usuario</Form.Label>
+                <Form.Label>Username</Form.Label>
                 <Form.Control type="text" value={username} onChange={handleInputChange} name="username" />
             </Form.Group>
 
@@ -62,6 +65,22 @@ const SignupForm = () => {
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" value={email} onChange={handleInputChange} name="email" />
             </Form.Group>
+
+            <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" value={name} onChange={handleInputChange} name="name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Image</Form.Label>
+                <Form.Control type="text" value={imageUrl} onChange={handleInputChange} name="imageUrl" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Bio</Form.Label>
+                <Form.Control type="text" value={bio} onChange={handleInputChange} name="bio" />
+            </Form.Group>
+
 
 
             <div className="d-grid">
