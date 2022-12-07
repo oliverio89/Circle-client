4ab53515b0d0a44748019dc037406bf879f4ef93
yoaomoreira <jpmoreiraes@gmail.com>
2022-12-07 13:24:20 +0100

    errormessage

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import LoginPage from "../pages/LoginPage/LoginPage"
 import SignupPage from './../pages/SignupPage/SignupPage'
-import PostListPage from './../pages/PostPage/PostPage'
+import PostListPage from '../pages/PostPage/PostPage'
 import PrivateRoute from "./PrivateRoute"
 
 
@@ -13,16 +13,16 @@ const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<p>Circle</p>} />
-            <Route path="/post" element={<PostListPage />} />
             <Route path="/aboutus" element={<p>Contacta con Nosotros</p>} />
             <Route path="/acceder" element={<LoginPage />} />
-            <Route path="/" element={<h1>404</h1>} />
+            <Route path="/*" element={<h1>404</h1>} />
             <Route path="/registro" element={<SignupPage />} />
-
+            {/* <Route path="/registro" element={<p>HIIIII</p>} /> */}
 
             <Route element={<PrivateRoute />}>
                 <Route path="/profile" element={<p>Perfil</p>} />
                 <Route path="/admin" element={<p>Panel de admin</p>} />
+                <Route path="/post" element={<PostListPage />} />
             </Route>
 
 
