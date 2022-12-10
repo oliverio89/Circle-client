e2c4b7feaf33bd5550e129fe85537f8982550ffc
yoaomoreira <jpmoreiraes@gmail.com>
2022-12-10 16:58:06 +0100

    profile css empezando

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -4,10 +4,12 @@ import { AuthContext } from '../../contexts/auth.context';
 import { useContext, useState } from 'react';
 import { Col, Container, Row, Modal } from 'react-bootstrap';
 import userService from '../../services/user.service'
+import LikeButton from '../../components/LikeButton/LikeButton';
 import EditProfileForm from '../../components/EditProfileForm/EditProfileForm';
-import postService from '../../services/post.service';
+import './ProfilePage.css'
 
 const addFriend = (user_id) => {
+    console.log('hiiiiii')
     userService
         .addFriend(user_id)
         .then(() => console.log(user_id))
@@ -15,6 +17,7 @@ const addFriend = (user_id) => {
 }
 
 function ProfilePage(name, bio, imageUrl, _id) {
+
 
     const navigate = useNavigate()
     const [showModal, setShowModal] = useState(false)
@@ -33,6 +36,7 @@ function ProfilePage(name, bio, imageUrl, _id) {
     }
 
     const addFriend = (user_id) => {
+        console.log('hiiiiii', user_id)
 
         userService
             .addFriend(user_id)
@@ -50,47 +54,47 @@ function ProfilePage(name, bio, imageUrl, _id) {
             }
             )
             .catch(err => console.error(err))
+
     }
 
     return (
-
         <Container>
-            <Row>
-                <Col md={{ span: 4 }}>
+            <Row className="d-none d-sm-none d-md-block d-lg-block profile">
+                <Col sm={4}>
                     <img src={user.imageUrl} style={{ width: '40%' }} />
+                    <h5>{user.name}</h5>
+                    <p>{user.bio}</p>
+                    <Button variant="" size="sm" onClick={editUser}>Editar Perfil</Button>
+                    <Button variant="" size="sm" onClick={() => deleteUser(user._id)}>Eliminar Perfil</Button>
                 </Col>
-                <Col>
-                    <p>{user.name}</p>
+                <Col sm={8}>
+                    <h4>Amigos</h4>
                     <p>{user.bio}</p>
                 </Col>
-
-                <h4>Amigos</h4>
-
-                {/* <Button as="div" variant="dark" onClick={() => addFriend(user._id)}>Agregar Amigo</Button> */}
-
-                <Col>
-                    <h4>Mis Publicaciones</h4>
-                    <p>{user.post_id}</p>
-
-                </Col>
-
-                <Modal show={showModal} onHide={closeModal}>
-
-                    <Modal.Header closeButton>
-                        <Modal.Title>Editar Perfil</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        {showForm === 'EditProfileForm' && <EditProfileForm name={user.name} bio={user.bio} imageUrl={user.imageUrl} id={user._id} />}
-                    </Modal.Body>
-                </Modal>
-
-                <Button as="div" variant="warning" onClick={editUser}>Editar Perfil</Button>
-                <Button variant="danger" size="sm" onClick={() => deleteUser(user._id)}>Eliminar Perfil</Button>
-                <Link to="/post">
-                    <Button as="div" variant="dark">Volver a las Publicaciones</Button>
-                </Link>
             </Row>
-        </Container>
+
+            {/* <Button as="div" variant="dark" onClick={() => addFriend(user._id)}>Agregar Amigo</Button> */}
+
+            <Col sm={12}>
+                <h4>Mis Publicaciones</h4>
+            </Col>
+
+            <Modal show={showModal} onHide={closeModal}>
+
+                <Modal.Header closeButton>
+                    <Modal.Title>Editar Perfil</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {showForm === 'EditProfileForm' && <EditProfileForm name={user.name} bio={user.bio} imageUrl={user.imageUrl} id={user._id} />}
+                </Modal.Body>
+            </Modal>
+
+            <Link to="/post">
+                <Button as="div" variant="dark">Volver a las Publicaciones</Button>
+            </Link>
+
+
+        </Container >
     );
 }
 
